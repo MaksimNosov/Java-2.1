93b64091b43439019cd71f64ddd957c34606ef8e
MaksimNosov <m.s.nosov@gmail.com>
2022-07-13 16:27:38 +0300

    Java 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        int TicketPrice = 2379;
+        int PriceForBonus = 20;
+        int BonusMile = TicketPrice / PriceForBonus;
+        System.out.println("Количество бонусов: " + BonusMile);
+    }
 }
