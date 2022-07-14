0111eee63f44369ef88913a5935cac0bdca8a438
MaksimNosov <m.s.nosov@gmail.com>
2022-07-14 14:27:25 +0300

    Small Letter

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int TicketPrice = 2379;
-        int PriceForBonus = 20;
-        int BonusMile = TicketPrice / PriceForBonus;
-        System.out.println("Количество бонусов: " + BonusMile);
+        int ticketPrice = 2379;
+        int priceForBonus = 20;
+        int bonusMile = ticketPrice / priceForBonus;
+        System.out.println("Количество бонусов: " + bonusMile);
     }
 }
